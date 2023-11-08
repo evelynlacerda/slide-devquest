3c0536130981f8f8419d9e5cedfe960b098e490f
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-11-08 20:50:17 -0300

    docs: add coments on javascript sheet

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@ let slidePosition = 0
 let slideAll = slideImage.length
 
 arrowNext.addEventListener('click', function() {
+    // Se a posição atual do slide for igual ao tamanho do array, o comando irá retornar, ou seja, não irá executar o código abaixo.
     if(slidePosition === slideAll - 1) {
         return;
     }
@@ -14,6 +15,7 @@ arrowNext.addEventListener('click', function() {
     slideRemoveOpacity();
     
     slidePosition++;
+    // Elemento do slide[posição do array feito na linha 4]
     slideImage[slidePosition].classList.add('show');
     
     arrowOpacity();
@@ -39,12 +41,15 @@ function slideRemoveOpacity() {
 
 function arrowOpacity() {
     if(slidePosition !== 0) {
+        // Se a posição atual do slide não for igual a 0, igual a primeira posição, o comando irá remover a classe de opacidade.
         arrowPrev.classList.remove('opacity');
     } else {
+        // Se a posição atual for igual a primeira posição, o comando irá adicionar a classe de opacidade.
         arrowPrev.classList.add('opacity');
     }
 
     if(slidePosition !== 0 && slidePosition === slideAll - 1) {
+        // Se a posição atual não for igual a 0, e a posição atual for identica ao tamanho do array, o comando irá adicionar a classe de opacidade.
         arrowNext.classList.add('opacity');
     } else {
         arrowNext.classList.remove('opacity');
